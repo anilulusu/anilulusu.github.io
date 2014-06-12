912cd293c25385c0e914a919b81f0a61ae1b0533
anilulusu <anilulusu@gmail.com>
2014-06-12 18:58:36 +0300

    JS dosyası ekl. ve güzelleşti

--- scr/tertip.js
+++ scr/tertip.js
@@ -1,5 +1,8 @@
+var cesit = "Cesitlerimiz iste bunlardır";
 var satir = "<div class='kav font-effect-anaglyph'>Kavacık!</div>";
+var ana;
 function renkli() {$(".tel").addClass("font-effect-anaglyph");}
 function ekle() {$("#logoMado > img").after(satir); $(".kav").fadeTo(2000,0.9); $(".kav").animate({"margin-top":"-110px"}, 100, "linear"); setTimeout(renkli,2000);}
-var cesit = "Cesitlerimiz iste bunlardır";
-function cesitler() {$("#cerceveOrta").html(cesit);}
+function cesitler() {$(".sak").hide();
+                    $("#cerceveOrta").append(document.getElementById("baslik"));}
+function home() {$("#yedek").append(document.getElementById("baslik")); $(".sak").show();}
