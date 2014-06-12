141dc6b6b1a10c779a2c351ef9be81b9e3a2c28f
anilulusu <anilulusu@gmail.com>
2014-06-12 23:40:16 +0300

    Otomat

--- scr/tertip.js
+++ scr/tertip.js
@@ -4,5 +4,8 @@ var ana;
 function renkli() {$(".tel").addClass("font-effect-anaglyph");}
 function ekle() {$("#logoMado > img").after(satir); $(".kav").fadeTo(2000,0.9); $(".kav").animate({"margin-top":"-110px"}, 100, "linear"); setTimeout(renkli,2000);}
 function cesitler() {$(".sak").hide();
-                    $("#cerceveOrta").append(document.getElementById("baslik"));}
-function home() {$("#yedek").append(document.getElementById("baslik")); $(".sak").show();}
+                    $("#cerceveOrta").append(document.getElementById("baslik"));
+                    $("#cerceveOrta").append(document.getElementById("urunler"));}
+function home() {$("#yedek").append(document.getElementById("baslik"));
+                 $("#yedek").append(document.getElementById("urunler"));
+                 $(".sak").show();}
