1c9f84eea31ffda47dd36d6801e6928732aeb0f2
anilulusu <anilulusu@gmail.com>
2014-06-12 15:25:58 +0300

    şekillendirme

---
+++ scr/tertip.js
@@ -0,0 +1,5 @@
+var satir = "<div class='kav font-effect-anaglyph'>Kavacık!</div>";
+function renkli() {$(".tel").addClass("font-effect-anaglyph");}
+function ekle() {$("#logoMado > img").after(satir); $(".kav").fadeTo(2000,0.9); $(".kav").animate({"margin-top":"-110px"}, 100, "linear"); setTimeout(renkli,2000);}
+var cesit = "Cesitlerimiz iste bunlardır";
+function cesitler() {$("#cerceveOrta").html(cesit);}
