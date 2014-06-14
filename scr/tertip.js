4d49cd3a952296421dc63c19e535277808472897
anilulusu <anilulusu@gmail.com>
2014-06-14 23:46:55 +0300

    sona yaklaşıyor

--- scr/tertip.js
+++ scr/tertip.js
@@ -1,11 +1,24 @@
 var cesit = "Cesitlerimiz iste bunlardır";
 var satir = "<div class='kav font-effect-anaglyph'>Kavacık!</div>";
-var ana;
 function renkli() {$(".tel").addClass("font-effect-anaglyph");}
 function ekle() {$("#logoMado > img").after(satir); $(".kav").fadeTo(2000,0.9); $(".kav").animate({"margin-top":"-110px"}, 100, "linear"); setTimeout(renkli,2000);}
-function cesitler() {$(".sak").hide();
-                    $("#cerceveOrta").append(document.getElementById("baslik"));
-                    $("#cerceveOrta").append(document.getElementById("urunler"));}
-function home() {$("#yedek").append(document.getElementById("baslik"));
-                 $("#yedek").append(document.getElementById("urunler"));
-                 $(".sak").show();}
+
+function cesitlero() {
+    var sak = document.getElementsByClassName("sak"), i = sak.length;
+    while(i--) {$("#yedek").append(sak[i]);
+    $("#cerceveOrta").load("urun.html");}
+}
+function cesitler() {
+    $("#yedek").append($("#cerceveOrta").contents());
+    $("#cerceveOrta").load("urun.html");
+}
+
+function homeo() {
+    $("#cerceveOrta").empty();
+    var sak = document.getElementsByClassName("sak"), i = sak.length;
+    while (i--) {$("#cerceveOrta").append(sak[i]);}
+}
+function home() {
+    $("#cerceveOrta").empty();
+    $("#cerceveOrta").append($("#yedek").contents());
+}
