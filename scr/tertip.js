6c247c320d3f5b73ce548fa29805fbf61787455d
anilulusu <anilulusu@gmail.com>
2014-06-22 22:24:51 +0300

    temizlik başlangıç

--- scr/tertip.js
+++ scr/tertip.js
@@ -4,28 +4,4 @@ function renkli() {
 }
 function ekle() {
     $("#logoMado > img").after(satir); $(".kav").fadeTo(2000,0.9); $(".kav").animate({"margin-top":"-110px"}, 100, "linear"); setTimeout(renkli,2000);
-}
-function cesitler() {
-    $("#yedek").append($("#cerceveOrta").contents());
-    $("#cerceveOrta").load("urun.html");
-}
-
-function yer() {
-    if($(".button").filter(":selected").text()==null) {
-        $("#yedek").append($("#cerceveOrta").contents());
-        $("#cerceveOrta").load("yerimiz.html");}
-    else{$("#cerceveOrta").load("yerimiz.html");}}
-
-
-function home() {
-    var isim = $("#yedek > div:first").attr("class");
-    if(isim == "sak") {
-    $("#cerceveOrta").empty();
-    $("#cerceveOrta").append($("#yedek").contents());
-    } 
-}
-
-function yaz() {
-    $(this).find("h1").clone().appendTo($("#baslik"));
-}
-    
+}    
