a447c6433760309f660d83cedf9bac1cb3f8f632
anilulusu <anilulusu@gmail.com>
2014-06-27 11:22:40 +0300

    disyaları ayıtdık - finalize

--- scr/tertip.js
+++ scr/tertip.js
@@ -5,4 +5,7 @@ function renkli() {
 function ekle() {
     $("#cerceveOrta").append($("#yedek").contents().clone());
     $("#logoMado > img").after(satir); $(".kav").fadeTo(2000,0.9); $(".kav").animate({"margin-top":"-110px"}, 100, "linear"); setTimeout(renkli,2000);
-}    
+    var query = queryToJSON().text();
+    alert(query);
+} 
+
