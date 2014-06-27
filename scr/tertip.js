6ac8ac678d78a9a0cdc8d5a10eccc186e98104a9
anilulusu <anilulusu@gmail.com>
2014-06-27 18:21:44 +0300

    biseyyok

--- scr/tertip.js
+++ scr/tertip.js
@@ -3,9 +3,9 @@ function renkli() {
     $(".tel").addClass("font-effect-anaglyph");
 }
 function ekle() {
-    $("#cerceveOrta").append($("#yedek").contents().clone());
     $("#logoMado > img").after(satir); $(".kav").fadeTo(2000,0.9); $(".kav").animate({"margin-top":"-110px"}, 100, "linear"); setTimeout(renkli,2000);
-    var query = queryToJSON().text();
-    alert(query);
+} 
+function ekle1() {
+    $("#logoMado > img").after(satir); $(".kav").fadeTo(2000,0.9); $(".kav").animate({"margin-top":"-110px"}, 100, "linear");
 } 
 
